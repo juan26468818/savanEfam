00e5dbb03f3d5f44f02bd3ba77749583d2f88a81
juan <juanayala@gmail.com>
2023-03-05 23:25:28 -0300

    minor changes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,13 +9,14 @@ const Home = ()=>{
     return(
         <div>
             <Header />
+            <GAuth />
             <div className="home__body">
                 <Photos class="Andres_2" title="Lorem Ipsum" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat metus sed est mollis, ut ultricies leo condimentum. Mauris sollicitudin massa a ligula tristique sollicitudin sit amet et enim. Nunc viverra, diam maximus rutrum varius, risus nulla efficitur neque, ac malesuada lorem ipsum eget urna. Duis lectus libero, tempus non tempor at, placerat a neque. Donec in fermentum urna. Quisque purus erat, pharetra consectetur elementum ut, convallis eu nulla. Nam laoreet aliquet feugiat. Sed mollis viverra massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut faucibus congue tempor."/>
                 <Photos class="Window_2" title="Lorem Ipsun" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat metus sed est mollis, ut ultricies leo condimentum. Mauris sollicitudin massa a ligula tristique sollicitudin sit amet et enim. Nunc viverra, diam maximus rutrum varius, risus nulla efficitur neque, ac malesuada lorem ipsum eget urna. Duis lectus libero, tempus non tempor at, placerat a neque. Donec in fermentum urna. Quisque purus erat, pharetra consectetur elementum ut, convallis eu nulla. Nam laoreet aliquet feugiat. Sed mollis viverra massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut faucibus congue tempor."/>
                 <Photos class="Food" title="Lorem Ipsun" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat metus sed est mollis, ut ultricies leo condimentum. Mauris sollicitudin massa a ligula tristique sollicitudin sit amet et enim. Nunc viverra, diam maximus rutrum varius, risus nulla efficitur neque, ac malesuada lorem ipsum eget urna. Duis lectus libero, tempus non tempor at, placerat a neque. Donec in fermentum urna. Quisque purus erat, pharetra consectetur elementum ut, convallis eu nulla. Nam laoreet aliquet feugiat. Sed mollis viverra massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut faucibus congue tempor."/>
             </div>
             <Footer />
-            <GAuth />
+            
         </div>
     )
 }
