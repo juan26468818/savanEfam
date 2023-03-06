8283d1a63dc8d2a0a2e21d5f7b6b6dbb28406535
juan <juanayala@gmail.com>
2023-03-05 23:23:08 -0300

    changed packages for auth

--- src/GAuth/gAuth.js
+++ src/GAuth/gAuth.js
@@ -1,22 +1,41 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+import { useEffect } from "react";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
+
+
+
+
 const GAuth = ()=>{
+    const clientID = "264216297261-hfrpjat26iviqtd9ckhcep5ilm4p4btd.apps.googleusercontent.com"
+
+    useEffect(() => {
+        const start = () =>{
+            gapi.auth2.init({
+                clientId: clientID,
+            })
+        }
+
+        gapi.load("client:auth2", start)
+        
+    }, []);
+
+    const onSuccess = (response) =>{
+        console.log(response)
+    }
+    const onFailure = (response) =>{
+        console.log(response)
+    }
+
+
     return(
-        <GoogleOAuthProvider clientId="264216297261-hfrpjat26iviqtd9ckhcep5ilm4p4btd.apps.googleusercontent.com">
-
-
-
-            <GoogleLogin
-            onSuccess={credentialResponse => {
-                console.log(credentialResponse);
-            }}
-            onError={() => {
-                console.log('Login Failed');
-            }}
-            auto_select
-            
+        <div>
+            <GoogleLogin 
+                clientId={clientID}
+                onSuccess= {onSuccess}
+                onFailure= {onFailure}
+                cookiePolicy={"single_host_policy"}
             />
-        </GoogleOAuthProvider>
+        </div>    
     );
 }
 
