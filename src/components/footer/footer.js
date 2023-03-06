c17d0efffe066fd681a6338742f2245d7f21840d
juan <juanayala@gmail.com>
2023-03-05 22:21:26 -0300

    Google Auth

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,9 +2,9 @@ import "./footer.css"
 const Footer = () =>{
     return(
         <footer>
-            <a href="#" class="social-container" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook-f"></i></a>
-            <a href="https://twitter.com/MafeNavas_" class="social-container" target="_blank" rel="noreferrer"><i class="fa-brands fa-twitter"></i></a>
-            <a href="https://www.instagram.com/savan.efam/?igshid=YmMyMTA2M2Y%3D" target="_blank" rel="noreferrer" class="social-container"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#" className="social-container" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="https://twitter.com/MafeNavas_" className="social-container" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com/savan.efam/?igshid=YmMyMTA2M2Y%3D" target="_blank" rel="noreferrer" className="social-container"><i className="fa-brands fa-instagram"></i></a>
         </footer>
     )
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,7 +2,7 @@ import "./home.css";
 import Header from "../../components/header/header";
 import Photos from "../../components/photos/photos";
 import Footer from "../../components/footer/footer";
-
+import GAuth from "../../GAuth/gAuth";
 
 
 const Home = ()=>{
@@ -15,6 +15,7 @@ const Home = ()=>{
                 <Photos class="Food" title="Lorem Ipsun" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat metus sed est mollis, ut ultricies leo condimentum. Mauris sollicitudin massa a ligula tristique sollicitudin sit amet et enim. Nunc viverra, diam maximus rutrum varius, risus nulla efficitur neque, ac malesuada lorem ipsum eget urna. Duis lectus libero, tempus non tempor at, placerat a neque. Donec in fermentum urna. Quisque purus erat, pharetra consectetur elementum ut, convallis eu nulla. Nam laoreet aliquet feugiat. Sed mollis viverra massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut faucibus congue tempor."/>
             </div>
             <Footer />
+            <GAuth />
         </div>
     )
 }
