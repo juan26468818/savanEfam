85b14f233f2391e6b9dbf144f24efbbb3834e81b
juan26468818 <juanayala@gmail.com>
2023-04-18 21:16:57 -0400

    footer change

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,8 +2,6 @@ import "./footer.css"
 const Footer = () =>{
     return(
         <footer>
-            <a href="#" className="social-container" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook-f"></i></a>
-            <a href="https://twitter.com/MafeNavas_" className="social-container" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a>
             <a href="https://www.instagram.com/savan.efam/?igshid=YmMyMTA2M2Y%3D" target="_blank" rel="noreferrer" className="social-container"><i className="fa-brands fa-instagram"></i></a>
         </footer>
     )
